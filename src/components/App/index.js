e02fd44bbd56ee2008e80b1d44aa3c9b85cfd030
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-01-23 22:19:27 +1100

    Remove passing of state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,14 @@
-import { useEffect, useState } from 'react';
-
 import styles from './App.module.css';
 import Header from '../Header';
 import Board from '../Board';
 import Keyboard from '../Keyboard';
 
 const App = () => {
-  const [ attempts, setAttempts ] = useState([[], [], [], [], [], []]);
-
-  const handleKeyUp = (event) => {
-    attempts[0].push(event.key);
-    console.log(event.key, attempts[0]);
-  };
-
   return (
-    <div className={ styles.App } onKeyUp={ handleKeyUp }>
-      { attempts }
+    <div className={ styles.App }>
       <Header />
-      <Board attempts={ attempts } />
-      <Keyboard setAttempts={ setAttempts } />
+      <Board />
+      <Keyboard />
     </div>
   );
 };
